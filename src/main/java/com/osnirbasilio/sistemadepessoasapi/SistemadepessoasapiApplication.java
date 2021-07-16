f91824b21e98919704152811ec1d12daca1287ac
Osnir Basílio <osnirxadrez@gmail.com>
2021-07-15 22:41:17 -0300

    Primeiro Commit

---
+++ src/main/java/com/osnirbasilio/sistemadepessoasapi/SistemadepessoasapiApplication.java
@@ -0,0 +1,13 @@
+package com.osnirbasilio.sistemadepessoasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadepessoasapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadepessoasapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osnirbasilio/sistemadepessoasapi/SistemadepessoasapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osnirbasilio.sistemadepessoasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadepessoasapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
